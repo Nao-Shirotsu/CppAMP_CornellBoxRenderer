db021d10f58982619ff618a0d69725b14d4861ae
Shirotsu <albus0sh@gmail.com>
2018-10-08 02:50:42 +0900

    レイと物体の表面の交差点情報を管理するクラスを実装

---
+++ CornellBoxRenderer/Intersection.cpp
@@ -0,0 +1,20 @@
+﻿#include "Intersection.hpp"
+#include "Constant.hpp"
+#include <cstdlib>
+
+Intersection::Intersection():
+	distance( Constant::INF ),
+	normalVec(),
+	pos(){
+	id = ++objNum;
+}
+
+Intersection::~Intersection(){
+	if( --objNum < 0 ){
+		std::exit( -1 );
+	}
+}
+
+int Intersection::GetID(){
+	return id;
+}
